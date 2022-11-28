6bfb233ba301f409e63494cf6c2decf83fd93fb1
AnmolSingh111 <aadirajput54321@gmail.com>
2022-11-28 21:41:14 +0530

    hogya kam

---
+++ Grading_system.py
@@ -0,0 +1,10 @@
+int_marks = 80
+
+if (int_marks > 90 and int_marks < 99):
+    print("A+")
+elif (int_marks > 80 and int_marks <= 90):
+    print("A")
+elif (int_marks > 70 and int_marks <= 80):
+    print("B+")
+elif int_marks > 60 and int_marks <= 70:
+    print('TERE BAS KA NI HAI')
