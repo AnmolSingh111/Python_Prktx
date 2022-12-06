b30a456f6bf3afa66826ec2172a8929ad934d04a
AnmolSingh111 <aadirajput54321@gmail.com>
2022-12-06 08:38:32 +0530

    5th dec morning

---
+++ Dictonary.py/dictonary.py
@@ -0,0 +1,27 @@
+# dictonary datastructure is like object or json format
+
+# dict1={'key':value}
+dict1={}
+dict1['ad']=11
+# dict1['ss']=11
+# dict1['ad']=11
+# print(dict1)
+# dict2={
+#     'name':'anmol','pind':'Gholowal','province':'Changar','pincode':174101
+# }
+# dict1['other stuff']=dict2
+print(dict1['ad'])
+# how to use dictonary as list 
+# dict1['22']={"asddsa"}
+print(dict1)
+dict1['skills']={"python":'js','gg':'hh'}
+dict1['khabar']={'Eyecolor':'black','nationality':'Indian'}
+dict1['list']=['j','322']
+
+print(dict1['list'][1])
+print(dict1['skills']['gg'])
+# new_list=dict1['details']
+# print(new_list[1]) ->this cant be done with object literal
+# if u want to access elements from new list ->
+print(dict1['khabar']['nationality'])
+
